31e1865462ac50a8c7c513ff10a7e7e33bda7961
Deckenlampe <FelixWissel@outlook.com>
2024-11-25 16:50:31 +0100

    Hausaufgabe 6

--- Hausaufgabe 6.py
+++ Hausaufgabe 6.py
@@ -1,22 +1,29 @@
 import numpy as np
 P = np.array([[1/3, 1/3, 1/3], [1/2, 1/4, 1/4], [1/2, 1/4, 1/4]])
+P_grenz = np.array([[2/5, 2/5, 1/5], [2/5, 2/5, 1/5], [2/5, 2/5, 1/5]])
 
 Q = np.array([[0.98, 0.02, 0],[0, 0.98, 0.02], [0.02, 0, 0.98]])
+Q_grenz = np.array([[1/3, 1/3, 1/3], [1/3, 1/3, 1/3], [1/3, 1/3, 1/3]])
 
-def matrix_diff(matrix):
+def matrix_diff(matrix, grenzmatrix):
     count = 0
-
+    matrix_copy = matrix.copy()
+    print(matrix_copy)
+    print(grenzmatrix)
     while True:
-        matrix_copy = matrix.copy()
-        matrix = matrix @ matrix  # Matrix multiplication
+        matrix = matrix @ matrix_copy  # Matrix multiplication
         count += 1
         # Compute the absolute difference between current and previous matrices
-        diff = np.abs(matrix - matrix_copy)
+        diff = np.abs(grenzmatrix - matrix)
         # Check if any element has changed by less than or equal to 0.001
         if np.all(diff <= 0.001):
             print("Number of multiplications:", count)
             print(diff)
             break
+        if count >= 500:
+            print("Could not find the number of multiplications (<500)")
+            print(matrix)
+            break
 
-matrix_diff(P)
-matrix_diff(Q)
+matrix_diff(P, P_grenz)
+matrix_diff(Q, Q_grenz)
